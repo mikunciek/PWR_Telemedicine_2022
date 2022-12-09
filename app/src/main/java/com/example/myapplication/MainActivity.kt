2a16cea934478a1c1655027a674c528c7a231f5a
Suvres <bartosz.blyszcz@gmail.com>
2022-12-09 15:38:16 +0100

    Utworzenie dwóch ścieżek nawigacyjnych

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.navigation.findNavController
 import com.example.myapplication.fragments.HomeFragment
+import com.example.myapplication.users.User
+import com.example.myapplication.users.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.*
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_caregiverpanel.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.tasks.await
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private var userRepository: UserRepository = UserRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +41,19 @@ class MainActivity : AppCompatActivity() {
         if (currentUser == null) {
             this.startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        userRepository.getUser(currentUser!!.uid).addOnSuccessListener {
+            val user = it.toObject(User::class.java);
+
+            if (user!!.caregiver.isEmpty()) {
+                this.nav_host_fragment.findNavController()
+                    .navigate(R.id.action_blankMenuFragment_to_menuCaregiverFragment)
+            } else {
+                this.nav_host_fragment.findNavController()
+                    .navigate(R.id.action_blankMenuFragment_to_menuPatientFragment)
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/myapplication/users/UserRepository.kt
+++ app/src/main/java/com/example/myapplication/users/UserRepository.kt
@@ -2,7 +2,16 @@ package com.example.myapplication.users
 
 import android.util.Log
 import com.example.myapplication.Repository
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.tasks.await
 
 class UserRepository: Repository() {
 
@@ -16,9 +25,27 @@ class UserRepository: Repository() {
 
     fun save(user: User) {
         cloud.collection(COLLECTION)
-            .document(user.uid!!)
+            .document(user.uid)
             .set(user);
 
         Log.d(FIREBASE_DEBUG, user.uid)
+    }
+
+    fun getUser(uid: String): Task<DocumentSnapshot>{
+
+        return cloud.collection(COLLECTION)
+            .document(uid).get()
+
+    }
+
+    fun createFromFirebaseUser(firebaseUser: FirebaseUser) {
+        val user = User(
+            uid = firebaseUser.uid,
+            email = firebaseUser.email ?: "",
+            firstName = firebaseUser.displayName ?: "",
+            phone = firebaseUser.phoneNumber,
+        )
+
+        this.save(user)
     }
 }
