55b8193d8aecea0ab5803e34d0504eab35f1dc68
mikunciek <mmikamonika@gmail.com>
2022-12-18 22:05:20 +0100

    dialogAlert, delete

--- app/src/main/java/com/example/myapplication/fragments/PatientsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/PatientsFragment.kt
@@ -1,27 +1,23 @@
 package com.example.myapplication.fragments
 
-import android.app.ProgressDialog.show
-import android.content.DialogInterface
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentPatientsBinding
-import com.example.myapplication.databinding.FragmentTodoCaregiverBinding
+import com.example.myapplication.databinding.ListPatientsBinding
 import com.example.myapplication.fragments.ToDoDialogFragment.Companion.TAG
 import com.example.myapplication.users.User
 import com.example.myapplication.users.UserRepository
-import com.example.myapplication.users.UserTask
+
 import com.example.myapplication.utils.adapter.PatientsAdapter
-import com.example.myapplication.utils.adapter.TaskAdapter
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -34,7 +30,13 @@ class PatientsFragment : Fragment() {
     private val userRepository = UserRepository()
     private lateinit var binding: FragmentPatientsBinding
     private lateinit var database: DatabaseReference
+
     private lateinit var buldier : AlertDialog.Builder
+
+    private lateinit var bindListView: ListPatientsBinding
+
+    private lateinit var button: ImageButton
+
 
 
     private lateinit var patientsAdapter: PatientsAdapter  //zadania
@@ -53,24 +55,33 @@ class PatientsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         init()
 
-            buldier = AlertDialog.Builder(requireContext())
+        buldier = AlertDialog.Builder(requireContext())
 
-           /* deletePatient.setOnClickListener{
+
+        button = deleteUser
+
+/*
+            button.setOnClickListener{
                 buldier
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Usuwanie pacjenta")
                 .setMessage("Czy chcesz usunąć pacjenta z listy użytkowników?")
                 .setCancelable(true) //dialog box in cancellable
-                .setPositiveButton("Tak") { dialog: DialogInterface?, which: Int ->
-
-                    //usunięcie użytkownika
+                .setPositiveButton("Tak") {dialogInterface, it ->
                     userRepository.deletePatients()
                 }
+                //{ dialog: DialogInterface?, which: Int -> userRepository.deletePatients()}
+
+                    //usunięcie użytkownika
+
                 .setNegativeButton("Nie"){dialogInterface, it ->dialogInterface.cancel() }
                 .show()
+
             }
 
-            */
+ */
+
+
         }
 
 
@@ -113,14 +124,9 @@ class PatientsFragment : Fragment() {
             binding.mainRecyclerView.adapter = patientsAdapter
 
             countPatients.text = String.format("Ilość Twoich pacjentów: %s ", patientsAdapter.itemCount.toString())
+
+
         }
-    }
-
-
-
-
-    fun onDeleteItemClick(){
-
     }
 
 
