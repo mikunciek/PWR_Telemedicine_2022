947e9204cb429cdec0aeef143522a9391f537e33
Suvres <bartosz.blyszcz@gmail.com>
2022-12-28 20:29:24 +0100

    Działający pdf

--- app/src/main/java/com/example/myapplication/fragments/MMSETestFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/MMSETestFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.fragments
 
 import android.media.Image
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,18 +11,28 @@ import android.webkit.WebView
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentMMSEtestBinding
+import com.github.barteksc.pdfviewer.PDFView
 import kotlinx.android.synthetic.main.fragment_guide.*
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.*
+import javax.net.ssl.HttpsURLConnection
 
 
 class MMSETestFragment : Fragment() {
-    private lateinit var binding: FragmentMMSEtestBinding
-    private var  webView: WebView = panel//?
-    val url =
-        "https://github.com/mikunciek/PWR_Telemedicine_2022/raw/master/pdf/MMSE.pdf"
+//    private lateinit var binding: FragmentMMSEtestBinding
+//    private var  webView: WebView = panel//?
+    lateinit var pdfView: PDFView
+    val url = "https://github.com/mikunciek/PWR_Telemedicine_2022/raw/master/pdf/MMSE.pdf"
 
     var mmseImageList = arrayListOf(R.drawable.mmse1,R.drawable.mmse2,
         R.drawable.mmse3,R.drawable.mmse4)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,33 +41,82 @@ class MMSETestFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_m_m_s_etest, container, false)
-
-
     }
 
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {  //właściwe wyświetlanie
         super.onViewCreated(view, savedInstanceState)
-        init()
+//        init()
+        pdfView = view.findViewById(R.id.idPDFView)
 
+        RetrievePDFFromURL(pdfView).execute(url)
         //TODO: Nie wiem czemu nie chce działać
-       backMainMenu.setOnClickListener{ findNavController().navigate(R.id.action_MMSETestFragment_to_menuCaregiverFragment) }
-
+//       view.findViewById(R.id.backMainMenu).setOnClickListener{ findNavController().navigate(R.id.action_MMSETestFragment_to_menuCaregiverFragment) }
     }
 
 
-    private fun init() {
+//    private fun init() {
+//
+//        webView.settings.javaScriptEnabled = true
+//        webView.settings.setSupportZoom(true)
+//        webView.isVerticalScrollBarEnabled = true
+//        webView.canGoBack()
+//        webView.canGoForward()
+//        webView.requestFocus()
+//        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$url")
+//
+//    }
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.setSupportZoom(true)
-        webView.isVerticalScrollBarEnabled = true
-        webView.canGoBack()
-        webView.canGoForward()
-        webView.requestFocus()
-        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$url")
+    // on below line we are creating a class for
+    // our pdf view and passing our pdf view
+    // to it as a parameter.
+    class RetrievePDFFromURL(pdfView: PDFView) :
+        AsyncTask<String, Void, InputStream>() {
 
+        // on below line we are creating a variable for our pdf view.
+        val mypdfView: PDFView = pdfView
+
+        // on below line we are calling our do in background method.
+        override fun doInBackground(vararg params: String?): InputStream? {
+            // on below line we are creating a variable for our input stream.
+            var inputStream: InputStream? = null
+            try {
+                // on below line we are creating an url
+                // for our url which we are passing as a string.
+                val url = URL(params.get(0))
+
+                // on below line we are creating our http url connection.
+                val urlConnection: HttpURLConnection = url.openConnection() as HttpsURLConnection
+
+                // on below line we are checking if the response
+                // is successful with the help of response code
+                // 200 response code means response is successful
+                if (urlConnection.responseCode == 200) {
+                    // on below line we are initializing our input stream
+                    // if the response is successful.
+                    inputStream = BufferedInputStream(urlConnection.inputStream)
+                }
+            }
+            // on below line we are adding catch block to handle exception
+            catch (e: Exception) {
+                // on below line we are simply printing
+                // our exception and returning null
+                e.printStackTrace()
+                return null;
+            }
+            // on below line we are returning input stream.
+            return inputStream;
+        }
+
+        // on below line we are calling on post execute
+        // method to load the url in our pdf view.
+        override fun onPostExecute(result: InputStream?) {
+            // on below line we are loading url within our
+            // pdf view on below line using input stream.
+            mypdfView.fromStream(result).load()
+
+        }
     }
-
 
 }
